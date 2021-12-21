c0c61e557a0d15882c76ac02a3fe86beec9f8d0a
Hendi Santika <hendisantika@gmail.com>
2021-12-21 10:46:25 +0700

    #9 - Add index in ContactController

--- src/main/java/com/hendisantika/controller/ContactController.java
+++ src/main/java/com/hendisantika/controller/ContactController.java
@@ -1,9 +1,14 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.model.Contact;
 import com.hendisantika.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ContactController {
     @Autowired
     private ContactRepository contactRepository;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Contact> contacts = contactRepository.findAll();
+        model.addAttribute("contacts", contacts);
+        return "index";
+    }
 }
